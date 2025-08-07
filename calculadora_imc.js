402249d1b0f07bd770629ff83cf551a9be666ac6
Lucas Nascimento <159586908+lukasnascky@users.noreply.github.com>
2025-08-06 21:17:18 -0300

    Create calculadora_imc.js

---
+++ calculadora_imc.js
@@ -0,0 +1,33 @@
+const peso = 75
+const altura = 1.75
+
+const imc = peso / altura ** 2
+
+console.log("Peso inserido: " + peso + " Kg")
+console.log("Altura inserida: " + altura + " m")
+console.log("IMC: " + imc)
+
+if (imc < 16){
+    console.log("Resultado: Magreza grave")
+} 
+else if (imc < 17){
+    console.log("Resultado: Magreza moderada")
+} 
+else if (imc < 18.5){
+    console.log("Resultado: Magreza leve")
+} 
+else if (imc < 25){
+    console.log("Resultado: Saudável")
+} 
+else if (imc < 30){
+    console.log("Resultado: Sobrepeso")
+} 
+else if (imc < 35){
+    console.log("Resultado: Obesidade grau I")
+} 
+else if (imc < 40){
+    console.log("Resultado: Obesidade grau II")
+} 
+else {
+    console.log("Resultado: Obesidade grau III")
+}
